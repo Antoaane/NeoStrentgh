d80141037a9aab786f26da84e934e579407bd7ba
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2023-11-12 21:34:50 +0100

    feat : reset structure, wip : animation's object initial placement

---
+++ src/components/StartAnimation.vue
@@ -0,0 +1,12 @@
+<script setup>
+    import NsIcon from "./icons/NsIcon.vue";
+</script>
+
+<template>
+    <div class="start-animation-container">
+        <div id="cover1"></div>
+        <div id="cover2"></div>
+        <NsIcon class="ns-icon" />
+    </div>
+    
+</template>
